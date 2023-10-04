a5f89d11f158af54ca9ab7bad55b4fc568e0ad48
Shaun LaFleur <shaunlafleur.kj@gmail.com>
2023-10-03 23:19:21 -0500

    Minor Changes
    Minor changes and optimizations. Was using an extra parameter and naming of variables wasn't optimal.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,12 +13,12 @@ class LinkedList {
     this.size = 0;
   }
 
-  // Add a node to the end
+  // Add a node to the end, redundant because the insertNode can do this with the correct parameter, but I left it in for a method that is explicitly named.
   appendNode(data) {
     this.insertNode(data, this.size+1); // calls the insertNode method to insert the new node to the end of the list
   }
 
-  // Add a node to the front of the list.
+  // Add a node to the front of the list. Redundant because the insertNode can do this with the correct parameter, but I left it in for a method that is explicitly named.
   prependNode(data) {
     this.insertNode(data, 1); // calls the insertNode method to insert the new node to the beginning of the list
   }
@@ -27,9 +27,9 @@ class LinkedList {
   insertNode(data, position) {
     let current = this.head;
     const newNode = new Node(data);
-    if(position > this.size+1 || position === 0 || isNaN(position)) { // We use this.size+1 because we want to allow inserting the new node to the end (if the list is 5 long, we can insert 6 to put it in position 6, ie at the end)
-      console.log("Invalid position.");
-      return; // Invalid position
+    if(position > this.size+1 || position === 0 || isNaN(position) || data === undefined) { // We use this.size+1 because we want to allow inserting the new node to the end (if the list is 5 long, we can insert 6 to put it in position 6, ie at the end)
+      console.log("Invalid position or empty data provided.");
+      throw new Error("Invalid position or empty data provided.");
     } else if(position === 1) { // If position is 1, we want to insert the new node at the start of the list into position 1
       if(this.head) { // And ONLY IF a head already exists, we
         this.head.previous = newNode; // set the current head's previous to the new node,
@@ -37,31 +37,31 @@ class LinkedList {
       }
       this.head = newNode; // and finally we set the newNode as the new head of the list irregarddless of whether there was already a head
     } else { // otherwise we will insert it at the given position, and we'll use a for loop to find that position
-      current = this.getNodeFromPosition(position-1); // We're going to find the position directly BEFORE the position we want to insert the new node, and then place the new node AFTER.
-      if(current.next) { // If there is a next node
-        current.next.previous = newNode; // we set the previous node of the current node's next node to the newNode,
-        newNode.next = current.next; // then set the newNode's next node to the current node's next node, since the new node will be placed after it
+      current = this.getNodeByPosition(position-1); // Grab the position directly BEFORE the position we want to insert the new node so we can place the new node AFTER this node.
+      if(current.next) { // If there is a node after the current node,
+        current.next.previous = newNode; // we set the "previous" property of the node that comes after the current node to reference the newly created node,
+        newNode.next = current.next; // then set the new node's "next" property to reference the node that comes after the current node, since the new node will be placed after the current node
       }
-      newNode.previous = current; // Finally, we set the previous node for the newNode as the current node and
-      current.next = newNode; // set the current node's next node to the newNode
+      newNode.previous = current; // Finally, we set the "previous" property of the new node to a reference to the current node and
+      current.next = newNode; // set the current node's "next" property to a reference of the new node.
     }
     this.size++;
   }
 
   listToConsole() {
     let current = this.head;
-    let count = 1;
+    let position = 1;
     while(current) {
-      console.log(`Node at position ${count}`, current);
+      console.log(`Node at position ${position}`, current);
       current = current.next;
-      count++;
+      position++;
     }
   }
 
-  getNodeFromPosition(position) {
+  getNodeByPosition(position) {
     if(position === 0 || this.size < position || isNaN(position)) {
       console.log("Invalid position.");
-      return;
+      throw new Error("Invalid position.");
     } else {
       let current = this.head;
       for(let i = 1; i <= position; i++) {
@@ -78,9 +78,8 @@ class LinkedList {
 const nodeDatabase = new LinkedList;
 
 function generateLinkedList(amount) {
-  for(let i = 0; i < amount; i++) {
-    const data = Math.floor(Math.random() * (100000 - 1 + 1)) + 1;
-    const newNode = new Node(data);
-    nodeDatabase.appendNode(newNode, i+1);
+  for(let i = 1; i <= amount; i++) {
+    const data = `This should be in position ${i}`; //Math.floor(Math.random() * (100000 - 1 + 1)) + 1;
+    nodeDatabase.appendNode(data);
   }
 }
